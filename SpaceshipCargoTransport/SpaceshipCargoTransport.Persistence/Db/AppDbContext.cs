324919bc1188f9b1f9b53671cf3ab92cc053a649
mizzer4 <tomaszmizerkiewicz@gmail.com>
2024-05-02 23:36:19 +0200

    Persistence service module defined

---
+++ SpaceshipCargoTransport/SpaceshipCargoTransport.Persistence/Db/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaceshipCargoTransport.Domain.Models;
+
+namespace SpaceshipCargoTransport.Persistence.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Spaceship> Spaceships { get; set; }
+    }
+}
---
+++ SpaceshipCargoTransport/SpaceshipCargoTransport.Persistence/Repositories/SpaceshipRepository.cs
@@ -0,0 +1,50 @@
+﻿using SpaceshipCargoTransport.Domain.Models;
+using SpaceshipCargoTransport.Domain.Repositories;
+using SpaceshipCargoTransport.Persistence.Db;
+
+namespace SpaceshipCargoTransport.Persistence.Repositories
+{
+    internal class SpaceshipRepository : ISpaceshipRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SpaceshipRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Create(Spaceship ship)
+        {
+            _context.Spaceships.Add(ship);
+            return SaveChanges();
+        }
+
+        public bool Delete(Spaceship ship)
+        {
+            _context.Spaceships.Remove(ship);
+            return SaveChanges();
+        }
+
+        public Spaceship? Get(Guid id)
+        {
+            return _context.Spaceships
+                .Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Spaceship> GetAll()
+        {
+            return _context.Spaceships.ToList();
+        }
+
+        public bool Update(Spaceship ship)
+        {
+            _context.Spaceships.Update(ship);
+            return SaveChanges();
+        }
+
+        private bool SaveChanges()
+        {
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
