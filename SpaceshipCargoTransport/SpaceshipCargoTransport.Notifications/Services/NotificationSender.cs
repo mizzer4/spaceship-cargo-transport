b65baeb4d677f37ec5856f95843a0e7bcb8f5aa7
mizzer4 <tomaszmizerkiewicz@gmail.com>
2024-05-04 22:49:38 +0200

    Set up notifications project

---
+++ SpaceshipCargoTransport/SpaceshipCargoTransport.Notifications/Services/NotificationSender.cs
@@ -0,0 +1,14 @@
+﻿using SpaceshipCargoTransport.Domain.Models;
+using SpaceshipCargoTransport.Domain.Notifications;
+
+namespace SpaceshipCargoTransport.Notifications.Services
+{
+    internal class NotificationSender : INotificationSender
+    {
+        public void Send(Transport transport, string message)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
